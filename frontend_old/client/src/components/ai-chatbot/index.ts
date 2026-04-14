9fa01290afc91bedee0e5c0bd53db52e3e3914c7
comage9 <comage9@gmail.com>
2026-04-14 23:57:45 +0900

    chore: 불필요한 프론트엔드 파일 정리 (client만 유지)

--- frontend_old/client/src/components/ai-chatbot/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as AIChatWidget } from './AIChatWidget';
-export { default as ChatMessage } from './ChatMessage';
-export { default as ChatInput } from './ChatInput';
-export { default as QuickQuestions } from './QuickQuestions';
--- frontend_old/client/src/main.tsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-console.log("main.tsx executing");
-createRoot(document.getElementById("root")!).render(<App />);
